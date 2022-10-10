08f2c47228718211794d06b018431447496c1fea
Sajid Mahamood <110928387+Sajid00173@users.noreply.github.com>
2022-10-10 19:55:52 +0600

    Add files via upload

---
+++ pai.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class pai {
+    public static void main(String[] args)
+    {
+        int num;
+        System.out.println("Enter the integer: ");
+
+        Scanner s = new Scanner(System.in);
+
+        num = s.nextInt();
+
+
+        System.out.println("Entered integer is: " + num);
+    }
+} 
